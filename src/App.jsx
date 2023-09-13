83592b0a7ce139703ec5887a3cbdd418b54b454f
Jinling Xi <jinlingxi@gmail.com>
2023-09-13 21:20:15 +0800

    support ssr on a single route

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { createRoot } from 'react-dom/client'
+import { useState } from 'react'
 import logo from './assets/images/logo.png'
 import './assets/style.css'
 
-function RunmixApp() {
+export default function RunmixApp() {
+  const [visible, setVisible] = useState(false)
+  const handleClick = () => setVisible(true)
   return (
     <div>
       <h1>Welcome to RunmixApp</h1>
-      <div>Add webpack-dev-server</div>
+      {visible && <div>You clicked button</div>}
+      <button onClick={handleClick}>Click me to test hydration</button>
       <img src={logo} style={{width: 60}} />
     </div>
   );
 }
-
-createRoot(document.getElementById('root')).render(<RunmixApp />)
---
+++ src/client.entry.jsx
@@ -0,0 +1,4 @@
+import { hydrateRoot } from 'react-dom/client';
+import RunmixApp from './App.jsx'
+
+hydrateRoot(document.getElementById('root'), <RunmixApp />);
