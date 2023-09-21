3ef0dfd289be394f0d5560c77d14c330ba27be1f
jinlingxi <jinlingxi@gmail.com>
2023-09-21 14:50:14 +0800

    fix: update nginx configuration

--- deploy/nginx/build.js
+++ deploy/nginx/build.js
@@ -7,10 +7,11 @@ async function main() {
   const template = fs.readFileSync(path.resolve(__dirname, 'nginx.conf.hbs'), 'utf8');
   const content = Handbars.compile(template)({
     domain: process.env.DEPLOY_DOMAIN,
+    port: process.env.SERVER_PORT,
     ssl: process.env.ssl === 'true',
     ssl_certificate: process.env.ssl_certificate,
     ssl_certificate_key: process.env.ssl_certificate_key,
-    root: path.resolve(__dirname, '../../dist') + '/',
+    root: path.resolve(__dirname, '../../dist') + '/',    
   })
   fs.writeFileSync(path.resolve(__dirname, 'nginx.conf'), content)
 }
